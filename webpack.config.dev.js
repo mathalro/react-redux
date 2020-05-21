dc6e610908399d24ede35d550164f3893baafd30
matheusp <matheusp@take.net>
2020-05-20 22:42:32 -0300

    first commit react-redux

---
+++ webpack.config.dev.js
@@ -0,0 +1,46 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map", // indicates that we can see the original code in the browser
+  entry: "./src/index",
+  output: {
+    // in development mode it will be in the memory, but it requires this folder
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true, // overlay any error that occurs in the browser
+    historyApiFallback: true, // all request will be sent to index.httml
+    // three last options is because an open issue in the last version of chrome
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
